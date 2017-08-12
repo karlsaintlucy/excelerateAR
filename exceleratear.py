f5a3853c0573c5af67f31e56d25d5947482a32fa
Karl Saint Lucy <karlsaintlucy@Harold-III.local>
2017-08-12 12:06:08 -0400

    Initial commit

---
+++ exceleratear.py
@@ -0,0 +1,433 @@
+"""excelerateAR for Idealist v0.1 by Karl Johnson.
+
+Usage (Unix): python3 exceleratear.py some-list-of-orgs.txt
+Usage (PC): python -m exceleratear some-list-of-orgs.txt
+
+where some-list-of-orgs is a path to a text file that contains
+a list of Idealist orgs (exact matches to names on the database),
+delimited by new lines.
+
+Note that in order to query Idealist v7's PostgreSQL database,
+psycopg2 expects credentials as a string with the following
+schema:
+
+"dbname='' user='' host='' password=''"
+
+with credential values in between single quotes.
+
+"""
+
+import datetime
+import json
+import os
+import re
+# import subprocess
+import sys
+# import time
+# import urllib.request
+
+# import pdfkit
+
+import psycopg2
+
+from termcolor import colored
+
+import xlsxwriter
+
+# Instantiate global variables
+user_name = None
+results_file = None
+excluded_orgs = None
+included_orgs = None
+data_dir = None
+right_now = None
+platform = None
+app_data = []
+orgs_balances = []
+
+
+def main():
+    """Run the higher-level tasks."""
+    try:
+        # Database credentials are stored in environment variable I7DB_CREDS
+        connect_str = os.environ["I7DB_CREDS"]
+    except:
+        print(colored("ERROR: " +
+                      "Need i7 database credentials exported as value " +
+                      "for I7DB_CREDS :(",
+                      "red"))
+        exit(1)
+
+    try:
+        source_file = sys.argv[1]
+    except:
+        print(colored("ERROR: " +
+                      "Need source file as command line argument :(",
+                      "red"))
+        exit(1)
+
+    # Get the user platform so directories can be created correctly
+    global platform
+    platform = os.name
+
+    # Fun li'l header
+    os.system('cls' if platform == 'nt' else 'clear')
+    print(colored("{:=^79s}".format(
+        "excelerateAR for Idealist, v0.1 by Karl Johnson"),
+        "white"))
+
+    # Get the user name that will be printed in the footer of the Excel file
+    global user_name
+    global right_now
+    global data_dir
+    global result_file
+    global excluded_orgs
+    global included_orgs
+
+    global app_data
+    global orgs_balances
+
+    print()
+
+    while True:
+        user_name = input(colored("Your name: ", 'white'))
+        if user_name:
+            break
+    print()
+
+    # Create a new subfolder for all the resulting files
+    right_now = datetime.datetime.now()
+
+    if not os.path.isdir("reports"):
+        os.mkdir("reports")
+
+    if platform == 'nt':
+        data_dir = r"reports\{} {}".format(
+            right_now.strftime("%Y %m%d %H%M%S"), user_name)
+        os.mkdir(data_dir)
+        os.mkdir(r"{}\docs".format(data_dir))
+        os.mkdir(r"{}\logs".format(data_dir))
+        results_file = open(r"{}\logs\data.json".format(data_dir), "a")
+        balances_file = open(r"{}\logs\balances.json".format(data_dir), "a")
+        included_orgs = open(r"{}\logs\included.txt".format(data_dir), "a")
+        excluded_orgs = open(r"{}\logs\excluded.txt".format(data_dir), "a")
+    else:
+        data_dir = r"reports/{} {}".format(
+            right_now.strftime("%Y %m%d %H%M%S"), user_name)
+        os.mkdir(data_dir)
+        os.mkdir(r"{}/docs".format(data_dir))
+        os.mkdir(r"{}/logs".format(data_dir))
+        results_file = open(r"{}/logs/data.json".format(data_dir), "a")
+        balances_file = open(r"{}/logs/balances.json".format(data_dir), "a")
+        included_orgs = open(r"{}/logs/included.txt".format(data_dir), "a")
+        excluded_orgs = open(r"{}/logs/excluded.txt".format(data_dir), "a")
+
+    print(colored("{:.<10s}".format("Running"), 'yellow'))
+    print()
+
+    excluded_count = 0
+    completed_count = 0
+
+    for line in open(source_file):
+        line = line.rstrip()
+
+        status = prepare_data(line, connect_str)
+        if status == 1:
+            exit(1)
+        elif status == 2:
+            print(colored(
+                "...EXCLUDED: {} - No rows returned"
+                .format(line), 'yellow'))
+            excluded_orgs.write(line + "\n")
+            excluded_count += 1
+        elif status == 3:
+            print(colored(
+                "...EXCLUDED: {} - Couldn't make Excel file"
+                .format(line), 'yellow'))
+            excluded_orgs.write(line + "\n")
+            excluded_count += 1
+        else:
+            print(colored("...OK: {}".format(line), 'cyan'))
+            included_orgs.write(line + "\n")
+            completed_count += 1
+
+    results_file.write(json.dumps(app_data, indent=4))
+    balances_file.write(json.dumps(orgs_balances, indent=4))
+
+    print()
+    print(colored("Process completed with {} completions and {} exclusions."
+                  .format(completed_count, excluded_count), 'green'))
+    print()
+
+
+def prepare_data(user_orgname, creds):
+    """Pull the data from the database and put it in data structures."""
+    try:
+        conn = psycopg2.connect(creds)
+        cursor = conn.cursor()
+
+        # Get the data
+        cursor.execute("""
+            SELECT row_number() over(ORDER BY i.number ASC),
+                i.number AS invoice_num,
+                'https://www.idealist.org/invoices/' || i.id
+                    AS invoice_link,
+                li.description AS description,
+                u.first_name || ' ' || u.last_name AS posted_by,
+                o.name AS org_name,
+                i.created::date AS posted_date,
+                (i.created + INTERVAL '45 days')::date AS due_date,
+                -- TODO: find where I got the below function and cite
+                EXTRACT(EPOCH FROM(SELECT(NOW() -
+                    (i.created + INTERVAL '45 days')))/86400)::int
+                    AS days_overdue,
+                li.unit_price AS amount_due
+            FROM invoices AS i
+            LEFT JOIN users AS u ON u.id = i.creator_id
+            LEFT JOIN orgs AS o ON o.id = i.org_id
+            LEFT JOIN line_items AS li ON li.invoice_id = i.id
+            WHERE o.name = %s
+            AND i.payment_settled = FALSE
+            AND ((li.item_type = 'JOB') OR (li.item_type = 'INTERNSHIP'))
+            AND EXTRACT(EPOCH FROM(SELECT(NOW() -
+                (i.created + INTERVAL '45 days')))/86400)::int > 0
+            ORDER BY i.number ASC;""", (user_orgname,))
+        rows = cursor.fetchall()
+    except:
+        print(colored("ERROR: \
+                       Something is wrong with your SQL, bro :(",
+                      "red"))
+        return 1
+
+    if not rows:
+        cursor.close()
+        conn.close()
+        return 2
+
+    cursor.close()
+    conn.close()
+
+    # Convert the SQL results to a dictionary and a bunch of JSON
+    i7_orgname = rows[0][5]
+
+    # To track balance for each org
+    org_balance = 0.0
+
+    # THIS BLOCK USES A DICTIONARY AND SPITS OUT RESULTS AS PRETTY JSON
+    # RETAINING FOR NOW, IN CASE THERE'S ANOTHER USE CASE USING JSON
+    keys = ["index",
+            "invoice_num",
+            "invoice_link",
+            "description",
+            "posted_by",
+            "org_name",
+            "posted_date",
+            "due_date",
+            "days_overdue",
+            "amount_due"]
+
+    results = [dict(zip(keys, values)) for values in rows]
+    for item in results:
+        item["description"] = re.search(
+            r"\"(.+)\"", item["description"]).group()[1:-1]
+        item["amount_due"] = float(item["amount_due"])
+        item["posted_date"] = item["posted_date"].strftime('%b %m, %Y')
+        item["due_date"] = item["due_date"].strftime('%b %m, %Y')
+        item["posted_by"] = item["posted_by"].title()
+        org_balance += item["amount_due"]
+
+    # Save the balance in the global list of org balances
+    orgs_balances.append({item["org_name"]: org_balance})
+
+    # Save the results dictionary to the global list of results
+    app_data.append(results)
+
+    # SAVE THE RESULTING DATA AS A BUNCH OF JSON IN A LOG FILE
+    # results_file.write(json.dumps(results, indent=4))
+
+    if make_excel(results, i7_orgname) == 0:
+        return 0
+    else:
+        return 3
+
+
+def make_excel(data, i7_orgname):
+    """Wrangle the data into an Excel file and a collection of PDFs."""
+    i7_orgname = re.sub("\/+", "-", i7_orgname)
+    i7_orgname = re.sub("\:+", "-", i7_orgname)
+
+    # CREATE THE EXCEL FILE
+    if platform == 'nt':
+        org_dir = r"{}\docs\{}".format(data_dir, i7_orgname)
+        if not os.path.isdir(org_dir):
+            os.mkdir(org_dir)
+        # org_docs = r"{}\docs".format(org_dir)
+        # if not os.path.isdir(org_docs):
+        #     os.mkdir(org_docs)
+        filename = r"{}\AWB Invoices - {} - {}.xlsx".format(
+            org_dir, i7_orgname, right_now.strftime('%b %d %Y'))
+    else:
+        org_dir = r"{}/docs/{}".format(data_dir, i7_orgname)
+        if not os.path.isdir(org_dir):
+            os.mkdir(org_dir)
+        # org_docs = r"{}/docs".format(org_dir)
+        # if not os.path.isdir(org_docs):
+        #     os.mkdir(org_docs)
+        filename = r"{}/AWB Invoices {} {}.xlsx".format(
+            org_dir, i7_orgname, right_now.strftime('%b %d %Y'))
+
+    wb = xlsxwriter.Workbook(filename)
+    ws = wb.add_worksheet("Overdue Invoices")
+
+    arial = {"font_name": "Arial Narrow"}
+    center = {"align": "center"}
+    big = {"font_size": 24}
+    medium = {"font_size": 18}
+    bold = {"bold": True}
+    italic = {"italic": True}
+    underline = {"underline": 1}
+    blue = {"font_color": "blue"}
+    gray = {"font_color": "gray"}
+    white = {"font_color": "white"}
+    black_bg = {"bg_color": "black"}
+    gray_bg = {"bg_color": "gray"}
+    money = {"num_format": 44}
+
+    # SET UP THE FORMATS TO BE USED
+    # I'm sure these can be consolidated at some point:
+    text_format = wb.add_format(
+        dict(arial))
+
+    title_format = wb.add_format(
+        dict(arial, **center, **big, **bold, **white, **black_bg))
+
+    subtitle_format = wb.add_format(
+        dict(arial, **center, **medium, **white, **black_bg))
+
+    header_format = wb.add_format(
+        dict(arial, **italic, **white, **gray_bg))
+
+    bold_format = wb.add_format(
+        dict(arial, **bold))
+
+    center_format = wb.add_format(
+        dict(arial, **center))
+
+    url_format = wb.add_format(
+        dict(arial, **center, **underline, **blue))
+
+    money_format = wb.add_format(
+        dict(arial, **money))
+
+    total_format = wb.add_format(
+        dict(arial, **bold, **money))
+
+    footer_format = wb.add_format(
+        dict(arial, **center, **italic))
+
+    maxcol = 7
+
+    # Write the title (org name)
+    row = 0
+    col = 0
+    ws.merge_range(row, col, row, maxcol, i7_orgname, title_format)
+
+    # Write the subtitle
+    row += 1
+    col = 0
+    ws.merge_range(row, col, row, maxcol, "Overdue Idealist invoices as of {}"
+                   .format(right_now.strftime("%b %d, %Y")), subtitle_format)
+
+    # Write the header row
+    row += 1
+    col = 0
+    col_widths = []
+
+    headers = ["Item No.",
+               "Invoice No.",
+               "Posting Title",
+               "Posted By",
+               "Posted Date",
+               "Due Date",
+               "Days Overdue",
+               "Amount Due"]
+
+    for header in headers:
+        width = len(str(header)) + 3
+        ws.set_column(col, col, width)
+        ws.write(row, col, header, header_format)
+        col_widths.append(width)
+        col += 1
+
+    # WRITE THE TABLE AND PDF DATA
+    row += 1
+    col = 0
+
+    for item in data:
+        # pdfkit.from_url(item["invoice_link"],
+        #                "{}.pdf"
+        #                .format(item["invoice_num"]))
+        # print(item["invoice_link"])
+        for key, value in item.items():
+            # Don't do anything with the "invoice_link" value
+            if key == "invoice_link" or key == "org_name":
+                continue
+
+            # Reset the column width if data value is wider than column header
+            width = len(str(value)) + 3
+            if width > col_widths[col]:
+                col_widths[col] = width
+
+            # Print invoice number as a hyperlink to the invoice on Idealist
+            if key == "invoice_num":
+                ws.write_url(row, col, item["invoice_link"],
+                             url_format, str(item["invoice_num"]))
+
+            # Center the index, invoice, and
+            elif key == "index" or key == "days_overdue":
+                ws.write_number(row, col, value, center_format)
+
+            # Print the amount due as a number
+            elif key == "amount_due":
+                ws.write_number(row, col, value, money_format)
+                # total_due += value
+
+            # Print all other values as normal text
+            else:
+                ws.write(row, col, str(value), text_format)
+            col += 1
+        row += 1
+        col = 0
+
+    # This is an ersatz block to automatically adjust the width
+    col = 0
+    for width in col_widths:
+        ws.set_column(col, col, width)
+        col += 1
+
+    # write the total
+    ws.write(row, 6, "Total:", bold_format)
+    ws.write_formula(row, 7, "=SUM(H4:H{})".format(row), total_format)
+
+    # Add the footer
+    row += 2
+    col = 0
+    ws.merge_range(row, col, row, maxcol, "Report run by {} at {} ET.".format(
+        user_name, right_now.strftime("%Y.%m.%d %I:%M:%S%p")), footer_format)
+    ws.merge_range(row + 1, col, row + 1, maxcol,
+                   "For more information, call (646) 786-6875, write " +
+                   "accountsreceivable@idealist.org, or go to your "
+                   "organization's dashboard on Idealist.",
+                   footer_format)
+
+    # Hide the gridlines for display
+    ws.hide_gridlines(2)
+
+    # Close the workbook
+    wb.close()
+
+    # All is good!
+    return 0
+
+if __name__ == "__main__":
+    main()
