be5020308d0a4bba3a5f9e2f66a868be0b9c7542
Karl Saint Lucy <karlsaintlucy@Harold-III.local>
2017-08-15 03:21:02 -0400

    Fix SQL to rule out paid line items

--- query.sql
+++ query.sql
@@ -10,12 +10,16 @@ SELECT row_number() over(ORDER BY i.number ASC),
         (i.created + INTERVAL '45 days')))/86400)::int AS days_overdue,
     li.unit_price AS amount_due
     FROM invoices AS i
-    LEFT JOIN users AS u ON u.id = i.creator_id
-    LEFT JOIN orgs AS o ON o.id = i.org_id
-    LEFT JOIN line_items AS li ON li.invoice_id = i.id
+    INNER JOIN users AS u ON u.id = i.creator_id
+    INNER JOIN orgs AS o ON o.id = i.org_id
+    INNER JOIN line_items AS li ON li.invoice_id = i.id
     WHERE o.name = %s
     AND i.payment_settled = FALSE
     AND ((li.item_type = 'JOB') OR (li.item_type = 'INTERNSHIP'))
     AND EXTRACT(EPOCH FROM(SELECT(NOW() -
         (i.created + INTERVAL '45 days')))/86400)::int > 0
+    --  https://stackoverflow.com/questions/3420982/opposite-of-inner-join
+    AND NOT EXISTS(SELECT NULL
+        FROM payment_parts AS pp
+        WHERE li.id = pp.line_item_id)
     ORDER BY i.number ASC;
