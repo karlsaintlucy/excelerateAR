e012169d9a6e2cf50913c20e581bf9b99ea1b2a3
Karl Saint Lucy <karlsaintlucy@Harold-III.local>
2017-08-15 01:36:52 -0400

    Externalize preferences and SQL query, fix db session open/close issue, atomize application functions.

---
+++ query.sql
@@ -0,0 +1,21 @@
+SELECT row_number() over(ORDER BY i.number ASC),
+    i.number AS invoice_num,
+    'https://www.idealist.org/invoices/' || i.id AS invoice_link,
+    li.description AS description,
+    u.first_name || ' ' || u.last_name AS posted_by,
+    o.name AS org_name,
+    i.created::date AS posted_date,
+    (i.created + INTERVAL '45 days')::date AS due_date,
+    EXTRACT(EPOCH FROM(SELECT(NOW() -
+        (i.created + INTERVAL '45 days')))/86400)::int AS days_overdue,
+    li.unit_price AS amount_due
+    FROM invoices AS i
+    LEFT JOIN users AS u ON u.id = i.creator_id
+    LEFT JOIN orgs AS o ON o.id = i.org_id
+    LEFT JOIN line_items AS li ON li.invoice_id = i.id
+    WHERE o.name = %s
+    AND i.payment_settled = FALSE
+    AND ((li.item_type = 'JOB') OR (li.item_type = 'INTERNSHIP'))
+    AND EXTRACT(EPOCH FROM(SELECT(NOW() -
+        (i.created + INTERVAL '45 days')))/86400)::int > 0
+    ORDER BY i.number ASC;
